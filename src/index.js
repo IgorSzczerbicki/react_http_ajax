2316a54bd3a2ded211eb80c0086cde1ce7d87964
Pl4cek <pl4cek@outlook.com>
2018-07-29 11:02:21 +0200

    Interceptory do globalnego wykonywania kodu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+let remove = false;
+
+const requestInterceptor = axios.interceptors.request.use(requestConfig => {
+	console.log(requestConfig);
+	//edytowanie konfiguracji zadania
+	return requestConfig;
+}, error => { //blad po stronie klienta
+	console.log(error);
+	return Promise.reject(error);
+});
+
+if (remove) axios.interceptors.request.eject(requestInterceptor); // usuwanie interceptora
+
+const responseInterceptor = axios.interceptors.response.use(responseConfig => {
+	console.log(responseConfig);
+	//edytowanie konfiguracji odpowiedzi
+	return responseConfig;
+}, error => { //blad po stronie serwera
+	console.log(error);
+	return Promise.reject(error);
+});
+
+if (remove) axios.interceptors.request.eject(responseInterceptor); // usuwanie interceptora
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
